44b1fd58e4009712cc67790bc330959fe611bb50
aibrindley <johnwbrindley@gmail.com>
2018-04-22 14:04:20 -0700

    adding profile picture and allowing project images to be seen on mo=bile

--- js/scripts.js
+++ js/scripts.js
@@ -89,8 +89,8 @@
     // Load additional projects
     $('#view-more-projects').click(function(e){
         e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
+        $(this).fadeOut(200, function() {
+            $('#more-projects').fadeIn(200);
         });
     });
 
